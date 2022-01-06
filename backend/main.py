5aa78cabb3f22a44a5fbd7e708eb48770443cb30
Donado Carlos <donado.carlos@bcg.com>
2022-01-06 23:17:40 +0100

    fix certificate storagw

--- backend/main.py
+++ backend/main.py
@@ -26,4 +26,4 @@ async def read_users_me(current_user: UserInfo = Depends(msal_auth.scheme)) -> U
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', reload=True)
